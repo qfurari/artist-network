8d3c094e1bf7c1f45f6e8854ca29851f822b31c0
ikkyu <ikkyuu.takiguchi@outlook.jp>
2025-05-14 18:12:15 +0900

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login.jsx";
+import Login from "./Login";
 import Callback from "./Callback";
+import Search from "./Search"; // ← これを忘れてると表示できない！
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/callback" element={<Callback />} />
-      <Route path="/search" element={<Search />} /> 
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/search" element={<Search />} /> {/* ← これが大事！ */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
